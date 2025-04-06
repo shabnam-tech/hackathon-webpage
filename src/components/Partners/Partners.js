f3d02cf6cb221d60cab31b5162c1e3a53e6ce3dc
shabnam-tech <nshabnam2006@gmail.com>
2025-04-06 18:19:02 +0530

    Modifications by shab

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,16 +1,57 @@
 import React from "react";
 import "./partners.css";
+import inno from './inno.png';
+import door from './door.jpg';
+import goorej from './goorej.jpg';
+import h2 from './h2.png';
+import moe from './moe.png';
+import sbi from './sbi.jpeg';
+import tcs from './tcs.png';
+import tutor from './tutor.jpg';
+import all from './all.jpg';
 
 const Partners = () => {
     return (
         <div className="partners-container">
-            <h2>Our Partners</h2>
-            <p>We are proudly partnered with some of the leading tech companies:</p>
-            <ul>
-                <li>Company A</li>
-                <li>Company B</li>
-                <li>Company C</li>
-            </ul>
+<h2 className="partners-title">Organisers</h2>
+  <div className="partners-container">
+    <div className="partners-row">
+      <div className="partner">
+        <img src={moe} alt="MoE's Innovation Cell" />
+      </div>
+      <div className="partner">
+        <img src={sbi} alt="SBI Foundation" />
+      </div>
+      <div className="partner">
+        <img src={inno} alt="Innovation for India" />
+      </div>
+    </div>
+    <div className="partners-row">
+      <div className="partner">
+        <h3>Official Partner</h3>
+        <img src={goorej} alt="Godrej Appliances" />
+      </div>
+      <div className="partner">
+        <h3>Evaluation Partner</h3>
+        <img src={tcs} alt="TCS" />
+      </div>
+      <div className="partner">
+        <h3>Learning Partner</h3>
+        <img src={tutor} alt="TutorialsPoint" />
+      </div>
+    </div>
+    <div className="partners-row">
+      <div className="partner">
+        <h3>Official Media Partner</h3>
+        <img src={all} alt="All India Radio" />
+        <img src={door} alt="Doordarshan" />
+      </div>
+      <div className="partner">
+        <h3>Platform Partner</h3>
+        <img src={h2} alt="Hack2Skill" className="large-partner" />
+      </div>
+    </div>
+  </div>
         </div>
     );
 };
--- src/components/Prizes/Prizes.js
+++ src/components/Prizes/Prizes.js
@@ -4,13 +4,30 @@ import "./prizes.css";
 const Prizes = () => {
     return (
         <div className="prizes-container">
-            <h2>Prizes</h2>
-            <ul>
-                <li>1st Place: $5000</li>
-                <li>2nd Place: $3000</li>
-                <li>3rd Place: $1500</li>
-            </ul>
-        </div>
+        <h2>Prizes & Rewards</h2>
+        <p className="prize-intro">Win exciting prizes and showcase your skills to top tech leaders!</p>
+      
+        <ul className="prize-list">
+          <li>
+            🥇 <strong>1st Place:</strong> $5000 + Trophy + Internship Opportunity with TCS
+          </li>
+          <li>
+            🥈 <strong>2nd Place:</strong> $3000 + Swags + Mentorship Session with Industry Experts
+          </li>
+          <li>
+            🥉 <strong>3rd Place:</strong> $1500 + Certificate of Excellence + Free Courses by TutorialsPoint
+          </li>
+          <li>
+            🚀 <strong>Best Innovation Award:</strong> Smart Gadgets + Special Mention on Website
+          </li>
+          <li>
+            🌐 <strong>Top 10 Finalists:</strong> E-Certificates + LinkedIn Endorsement + Spotlight on Social Media
+          </li>
+        </ul>
+      
+        <p className="prize-note">All participants will receive a certificate of participation.</p>
+      </div>
+      
     );
 };
 
